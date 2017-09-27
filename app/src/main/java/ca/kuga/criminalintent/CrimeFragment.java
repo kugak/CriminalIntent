8215ad4cea56bd638c6788d7ca607aa90c8700ae
Kugak <contact@kuga.ca>
2017-09-27 09:36:20 -0400

    added CrimeFragment class with & added and linked TextField

---
+++ app/src/main/java/ca/kuga/criminalintent/CrimeFragment.java
@@ -0,0 +1,65 @@
+package ca.kuga.criminalintent;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+/**
+ * Fragment Controller
+ * Present the details of a specific crime and
+ * update those details as the user changes them.
+ */
+
+public class CrimeFragment extends Fragment {
+
+    private Crime mCrime;
+    private EditText mTitleField;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mCrime = new Crime();
+    }
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        //expicitly inflate fragments view
+        View v = inflater.inflate(R.layout.fragment_crime, container, false);
+
+        //referencing to EditText
+        mTitleField = (EditText) v.findViewById(R.id.crime_title);
+
+        //adding EditText Listener
+        mTitleField.addTextChangedListener(new TextWatcher()
+        {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // intentional blank
+            }
+
+            //this returns string from user, setting to crimeTitle
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mCrime.setmTitle(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // intentional blank
+
+            }
+        });// end of addTextChangedListener
+
+
+
+        return v;
+
+    }
+}
