0c3e3bf1dbaaa6555e68702de486004a46d1b91b
Kugak <contact@kuga.ca>
2017-09-28 12:26:56 -0400

    renamed activity_crime.xml to activity_fragment.xml

--- app/src/main/java/ca/kuga/criminalintent/CrimeActivity.java
+++ app/src/main/java/ca/kuga/criminalintent/CrimeActivity.java
@@ -10,7 +10,7 @@ public class CrimeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_crime);
+        setContentView(R.layout.activity_fragment);
 
         //getting fragment manager (beacuse of support)
         FragmentManager fm = getSupportFragmentManager();
