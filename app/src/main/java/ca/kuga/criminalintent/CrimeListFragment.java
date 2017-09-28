bc84ba1b4275716c503d787532a1a27af3a7e580
Kugak <contact@kuga.ca>
2017-09-28 14:23:55 -0400

    added recycler view

--- app/src/main/java/ca/kuga/criminalintent/CrimeListFragment.java
+++ app/src/main/java/ca/kuga/criminalintent/CrimeListFragment.java
@@ -1,11 +1,34 @@
 package ca.kuga.criminalintent;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Controller Crime List Fragment
  */
 
 public class CrimeListFragment extends Fragment {
+
+    //set up view for recycler view
+    private RecyclerView mCrimeRecyclerView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_crime_list, container, false);
+
+        //delegates job to layoutManger
+        mCrimeRecyclerView = (RecyclerView) view.findViewById(R.id.crime_recycler_view);
+        //positions each item and defines scrolling
+        mCrimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        return view;
+    }
 
 }
