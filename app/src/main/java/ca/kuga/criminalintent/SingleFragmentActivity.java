21ea058966fc9b01f94108255aae300f3968dcbb
Kugak <contact@kuga.ca>
2017-09-28 12:30:08 -0400

    added singleFragmentActivity (abstract class) for fragments

---
+++ app/src/main/java/ca/kuga/criminalintent/SingleFragmentActivity.java
@@ -0,0 +1,11 @@
+package ca.kuga.criminalintent;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Abstract class for SingleFragment
+ */
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+
+}
