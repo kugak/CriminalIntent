a06379895ad68979fab8a28f246a510a5f95791a
Kugak <contact@kuga.ca>
2017-10-05 20:09:05 -0400

    added new intent method to pass crime data

--- app/src/main/java/ca/kuga/criminalintent/CrimeActivity.java
+++ app/src/main/java/ca/kuga/criminalintent/CrimeActivity.java
@@ -1,8 +1,22 @@
 package ca.kuga.criminalintent;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 
+import java.util.UUID;
+
 public class CrimeActivity extends SingleFragmentActivity {
+
+    public static final String EXTRA_CRIME_ID =
+            "ca.kuga.criminalintent.crime_id";
+
+    //new Intent method
+    public static Intent newIntent(Context packageContext, UUID crimeId) {
+        Intent intent = new Intent(packageContext, CrimeActivity.class);
+        intent.putExtra(EXTRA_CRIME_ID, crimeId);
+        return intent;
+    }
 
     @Override
     protected Fragment createFragment () {
