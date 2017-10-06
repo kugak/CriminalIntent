cfaba78847dc9e147ef1e0a02e200b5d58d4d1fd
Kugak <contact@kuga.ca>
2017-10-05 21:26:44 -0400

    added view pager activity and required class for it

---
+++ app/src/main/java/ca/kuga/criminalintent/CrimePageActivity.java
@@ -0,0 +1,17 @@
+package ca.kuga.criminalintent;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * View Pager
+ */
+
+public class CrimePageActivity extends AppCompatActivity {
+
+    //setting up viewpager
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime_pager);
+}
