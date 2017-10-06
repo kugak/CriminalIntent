b4bc7549706bfa87a7738a1a5247e12292cd857c
Kugak <contact@kuga.ca>
2017-10-05 21:29:59 -0400

    setting up adapter

--- app/src/main/java/ca/kuga/criminalintent/CrimePageActivity.java
+++ app/src/main/java/ca/kuga/criminalintent/CrimePageActivity.java
@@ -1,7 +1,13 @@
 package ca.kuga.criminalintent;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+
+import java.util.List;
 
 /**
  * View Pager
@@ -9,9 +15,34 @@ import android.support.v7.app.AppCompatActivity;
 
 public class CrimePageActivity extends AppCompatActivity {
 
+    private ViewPager mViewPager;
+    private List<Crime> mCrimes;
+
     //setting up viewpager
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime_pager);
+
+        //setting up page adapter
+        mViewPager = (ViewPager) findViewById(R.id.crime_view_pager);
+
+        mCrimes = CrimeLab.get(this).getCrimes();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        mViewPager.setAdapter(new FragmentStatePagerAdapter(fragmentManager) {
+
+            @Override
+            public Fragment getItem(int position) {
+                Crime crime = mCrimes.get(position);
+                return CrimeFragment.newInstance(crime.getmID());
+            }
+
+            @Override
+            public int getCount() {
+                return mCrimes.size();
+            }
+        });
+
+
+    }
 }
